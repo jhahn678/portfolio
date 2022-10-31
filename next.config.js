99ad48e638e53046144e5b2eb7a96c0eae31345f
jhahn678 <jhahn678@yahoo.com>
2022-10-30 22:15:00 -0400

    allowed image urls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['julian-hahn-portfolio.s3.amazonaws.com'],
+    
+  }
 }
 
 module.exports = nextConfig
