40027a3590464ecd73f86491f90f22d4315ab96b
jhahn678 <jhahn678@yahoo.com>
2022-11-02 11:53:10 -0400

    refactored row of buttons into comp

---
+++ components/buttons/ContactButtons/ContactButtons.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { BsEnvelope, BsPhone } from "react-icons/bs";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import { useModalStore } from "../../../hooks/useModalStore";
+import { ModalType } from "../../../utils/modalContent";
+import IconButton from "../IconButton/IconButton";
+import styles from './ContactButtons.module.css'
+
+interface Props{
+    className?: string | undefined
+}
+
+const ContactButtons = ({ className }: Props) => {
+
+    const setModal = useModalStore(store => store.setModal)
+    const handlePhone = () => setModal(ModalType.Contact)
+    
+  return (
+    <div className={`${styles.iconButtonContainer} ${className}`}>
+        <a href="https://github.com/jhahn678" target='_blank'>
+            <IconButton 
+            // small={maxWidth1050 ? true : false}
+            icon={<IoLogoGithub size={32} color={'var(--orangeLight)'}/>}
+            />
+        </a>
+        <a href="https://www.linkedin.com/in/julian-hahn-440527232/" target='_blank'>
+            <IconButton 
+            // small={maxWidth1050 ? true : false}
+            icon={<IoLogoLinkedin size={32} color={'var(--orangeLight)'}/>}
+            />
+        </a>
+        <IconButton 
+            // small={maxWidth1050 ? true : false}
+            icon={<BsPhone size={32} color={'var(--orangeLight)'}/>} 
+            onClick={handlePhone}
+        />
+        <a href='mailto:jhahn678@yahoo.com'>
+            <IconButton 
+            // small={maxWidth1050 ? true : false}
+            icon={<BsEnvelope size={32} color={'var(--orangeLight)'}/>}
+            />
+        </a>
+    </div> 
+  );
+};
+
+export default ContactButtons;
