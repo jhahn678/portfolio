4223150e7c0272acba0d35a7502ecbf5a1673b66
jhahn678 <jhahn678@yahoo.com>
2022-11-02 11:49:05 -0400

    setup axios with contentful url

--- config/axios.ts
+++ config/axios.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios'
 
 Axios.defaults.baseURL = process.env.CONTENTFUL_GRAPH_URL
+Axios.defaults.headers.common = { "Authorization": `Bearer ${process.env.CONTENT_PREVIEW_ACCESS_TOKEN!}`}
 
 export default Axios
