5c2fff8c5691628da399c195ca90962618a1e003
jhahn678 <jhahn678@yahoo.com>
2022-11-02 11:51:36 -0400

    added additional svg mapping

--- utils/techStackLogo.ts
+++ utils/techStackLogo.ts
@@ -4,5 +4,6 @@ export const stackItemToLogo = (stack: StackItem): string => {
     if(stack === 'Google Cloud') return 'googlecloud';
     if(stack === 'Next.js') return 'vercel';
     if(stack === 'React Native') return 'react';
+    if(stack === 'HTML & CSS') return 'html';
     return stack.toLowerCase();
 }
